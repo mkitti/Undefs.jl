051d61a18c5fe44e0e929052b17e38b953f81994
Mark Kittisopikul <markkitt@gmail.com>
2023-01-24 21:55:50 -0500

    Implement Base.show for JLArray

--- src/JLArrays.jl
+++ src/JLArrays.jl
@@ -64,4 +64,34 @@ function isptrarray(array::Array)
     return jla.ptrarray
 end
 
+function Base.show(io::IO, ::MIME"text/plain", jla::JLArray)
+    println(io, typeof(jla), ":")
+    println(io, "   data: $(jla.data)")
+    println(io, " length: $(jla.length)")
+    println(io, "  flags: $(bitstring(jla.flags))")
+    println(io, "        how: $(jla.how) ($(how(jla.how)))")
+    println(io, "      ndims: $(jla.ndims)")
+    println(io, "     pooled: $(jla.pooled)")
+    println(io, "   ptrarray: $(jla.ptrarray)")
+    println(io, "   isshared: $(jla.isshared)")
+    println(io, "  isaligned: $(jla.isaligned)")
+    println(io, " elsize: $(jla.elsize)")
+    println(io, " offset: $(jla.offset)")
+    println(io, "  nrows: $(jla.nrows)")
+    if jla.ndims == 1
+        println(io, "maxsize: $(jla.ncols)")
+    else
+        println(io, "  ncols: $(jla.ncols)")
+    end
+    println(io, "  other: $(jla.other)")
+end
+
+function how(i)
+    i == 0 ? "data is inlined, or a foreign pointer we don't manage" :
+    i == 1 ? "julia-allocated buffer that needs to be marked" :
+    i == 2 ? "malloc-allocated pointer this array object manages" :
+    i == 3 ? "has a pointer to the object that owns the data" :
+    error("Unknown how: $how")
+end
+
 end
